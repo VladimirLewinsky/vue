e157942328882a3134a39868934ab6fc2dcc9e7b
Vladimir <112421307+VladimirLewinsky@users.noreply.github.com>
2024-05-26 17:31:02 +0300

    Add files via upload

---
+++ homework3/ProductDetails.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <p>{{ product.name }}</p>
+    <p>{{ formattedPrice}}</p>
+    <template v-if="product.available">
+        Available
+    </template>
+    <template v-if="!product.available">
+        Out of stock
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueJsProductDetails',
+
+  data() {
+    return {
+      
+        product:{
+            name:"Banana",
+            price: 100,
+            available:false,
+        }
+    };
+  },
+
+  computed:{
+    formattedPrice(){
+        return "$"+ String(this.product.price)
+    }
+
+  },
+
+  methods: {
+   
+  },
+};
+</script>
