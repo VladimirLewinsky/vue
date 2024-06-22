198e60cc8c6f390207a3f2e96a1543649da03ab2
Vladimir <112421307+VladimirLewinsky@users.noreply.github.com>
2024-06-23 01:42:18 +0300

    Add files via upload

---
+++ homework3/homework_3.1/comp_details.js
@@ -0,0 +1,183 @@
+Vue.component("new-Kitchen", {
+  template: `<div class="main_container__articles_comp">
+          <h1 class="main_container__articles_comp__title">Let’s Get Solution for Building Construction Work</h1>
+           <img src="./img/details/1.png" alt="img">
+          <div class="main_container__articles_comp_block">
+            <p class="main_container__articles_comp_block__text">26 December,2022 </p>
+            <p class="main_container__articles_comp__text">Interior / Home / Decore</p>
+          </div>
+          <p class="main_container__articles_comp_article">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.<br>
+          <br>
+          Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.
+          </p>
+          <div class="main_container__articles_comp_slogan">
+               <img src="./img/details/“.png" alt="">
+            <p class="main_container__articles_comp_slogan__text">The details are not the details. 
+              They make the design.
+            </p>
+          </div>
+
+          <h1 class="main_container__articles_comp__title">
+          Design sprints are great
+          </h1>
+          <p class="main_container__articles_comp__content"> Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae     turpmaximus. posuere in.Contrary to popular belief.There are many variations of   passages of Lorem Ipsum available, but the majority have suffered.
+          </p>
+          <ol  class="main_container__articles_comp__list main_container__articles_comp__content" >
+
+            <li class="main_container__articles_comp__list__count        main_container__articles_comp__content"> Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered. </li>
+
+            <li class="main_container__articles_comp__list__count main_container__articles_comp__content"> Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered. </li>
+
+            <li class="main_container__articles_comp__list__count main_container__articles_comp__content"> Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered. </li>
+          </ol>
+
+          <img  src="./img/details/2.png" alt="img">
+          <p class="main_container__articles_comp__lasttext">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered.
+          </p>
+          </div>`,
+});
+
+Vue.component("new-Bedroom", {
+  template: `<div class="main_container__articles_comp">
+          <h1 class="main_container__articles_comp__title">About Bedroom</h1>
+           <img src="./img/details/Image_5.png" alt="img">
+          <div class="main_container__articles_comp_block">
+            <p class="main_container__articles_comp_block__text">26 December,2022 </p>
+            <p class="main_container__articles_comp__text">Interior / Home / Decore</p>
+          </div>
+          <p class="main_container__articles_comp_article">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.<br>
+          <br>
+          Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.
+          </p>
+          <div class="main_container__articles_comp_slogan">
+               <img src="./img/details/“.png" alt="">
+            <p class="main_container__articles_comp_slogan__text">The details are not the details. 
+              They make the design.
+            </p>
+          </div>
+
+          <h1 class="main_container__articles_comp__title">
+          Design sprints are great
+          </h1>
+          <p class="main_container__articles_comp__content"> Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae     turpmaximus. posuere in.Contrary to popular belief.There are many variations of   passages of Lorem Ipsum available, but the majority have suffered.
+          </p>
+          <ol  class="main_container__articles_comp__list main_container__articles_comp__content" >
+
+            <li class="main_container__articles_comp__list__count        main_container__articles_comp__content"> Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered. </li>
+
+            <li class="main_container__articles_comp__list__count main_container__articles_comp__content"> Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered. </li>
+
+            <li class="main_container__articles_comp__list__count main_container__articles_comp__content"> Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered. </li>
+          </ol>
+
+          <img  src="./img/details/image_3.png" alt="img">
+          <p class="main_container__articles_comp__lasttext">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered.
+          </p>
+          </div>`,
+});
+Vue.component("new-Building", {
+  template: `<div class="main_container__articles_comp">
+          <h1 class="main_container__articles_comp__title">MY Building Construction Work</h1>
+           
+         
+          <p class="main_container__articles_comp_article">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.<br>
+          <br>
+          Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.
+          </p>
+          <div class="main_container__articles_comp_slogan">
+               <img src="./img/details/“.png" alt="">
+            <p class="main_container__articles_comp_slogan__text">The details are not the details. 
+              They make the design.
+            </p>
+          </div>
+
+          <h1 class="main_container__articles_comp__title">
+          Design sprints are great
+          </h1>
+          <p class="main_container__articles_comp__content"> Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae     turpmaximus. posuere in.Contrary to popular belief.There are many variations of   passages of Lorem Ipsum available, but the majority have suffered.
+          </p>
+          <ol  class="main_container__articles_comp__list main_container__articles_comp__content" >
+
+            <li class="main_container__articles_comp__list__count        main_container__articles_comp__content"> Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered. </li>
+
+            <li class="main_container__articles_comp__list__count main_container__articles_comp__content"> Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered. </li>
+
+            <li class="main_container__articles_comp__list__count main_container__articles_comp__content"> Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered. </li>
+          </ol>
+
+          <img  src="./img/details/2.png" alt="img">
+          <p class="main_container__articles_comp__lasttext">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered.
+          </p>
+          </div>`,
+});Vue.component("new-Architecture", {
+  template: `<div class="main_container__articles_comp">
+          <h1 class="main_container__articles_comp__title">New Vision on Architecture</h1>
+           <img src="./img/details/arch.jpg" alt="img">
+          
+          <p class="main_container__articles_comp_article">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.<br>
+          <br>
+          Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.
+          </p>
+          <div class="main_container__articles_comp_slogan">
+               <img src="./img/details/“.png" alt="">
+            <p class="main_container__articles_comp_slogan__text">The details are not the details. 
+              They make the design.
+            </p>
+          </div>
+
+          <h1 class="main_container__articles_comp__title">
+          Design sprints are great
+          </h1>
+          <p class="main_container__articles_comp__content"> Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae     turpmaximus. posuere in.Contrary to popular belief.There are many variations of   passages of Lorem Ipsum available, but the majority have suffered.
+          </p>
+          <ol  class="main_container__articles_comp__list main_container__articles_comp__content" >
+
+            <li class="main_container__articles_comp__list__count        main_container__articles_comp__content"> Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered. </li>
+
+            <li class="main_container__articles_comp__list__count main_container__articles_comp__content"> Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered. </li>
+
+            <li class="main_container__articles_comp__list__count main_container__articles_comp__content"> Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered. </li>
+          </ol>
+
+          <img  src="./img/details/2.png" alt="img">
+          <p class="main_container__articles_comp__lasttext">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered.
+          </p>
+          </div>`,
+});Vue.component("new-KitchenPlanning", {
+  template: `<div class="main_container__articles_comp">
+          <h1 class="main_container__articles_comp__title">Let’s Get Solution for Building Construction Work</h1>
+           <img src="./img/details/2.png" alt="img">
+          <div class="main_container__articles_comp_block">
+            <p class="main_container__articles_comp_block__text">26 December,2022 </p>
+            <p class="main_container__articles_comp__text">Interior / Home / Decore</p>
+          </div>
+          <p class="main_container__articles_comp_article">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.<br>
+          <br>
+          Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.
+          </p>
+          <div class="main_container__articles_comp_slogan">
+               <img src="./img/details/“.png" alt="">
+            <p class="main_container__articles_comp_slogan__text">The details are not the details. 
+              They make the design.
+            </p>
+          </div>
+
+          <h1 class="main_container__articles_comp__title">
+          Design sprints are great
+          </h1>
+          <p class="main_container__articles_comp__content"> Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae     turpmaximus. posuere in.Contrary to popular belief.There are many variations of   passages of Lorem Ipsum available, but the majority have suffered.
+          </p>
+          <ol  class="main_container__articles_comp__list main_container__articles_comp__content" >
+
+            <li class="main_container__articles_comp__list__count        main_container__articles_comp__content"> Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered. </li>
+
+            <li class="main_container__articles_comp__list__count main_container__articles_comp__content"> Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered. </li>
+
+            <li class="main_container__articles_comp__list__count main_container__articles_comp__content"> Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered. </li>
+          </ol>
+
+          <img  src="./img/details/image_3.png" alt="img">
+          <p class="main_container__articles_comp__lasttext">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered.
+          </p>
+          </div>`,
+});
